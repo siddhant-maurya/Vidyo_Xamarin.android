ffbb8b63ca9d4b5670fc22ea8ae153c16e70d84d
Siddhant Maurya <siddhant.maurya@taritas.com>
2019-07-23 16:58:30 +0530

    Added solution

---
+++ VidyoDemo/VidyoDemo.Android/MainActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Android.App;
+using Android.Content.PM;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace VidyoDemo.Droid
+{
+    [Activity(Label = "VidyoDemo", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : Activity
+    {
+        VidyoController vc = new VidyoController();
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            
+
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.Main);
+
+            Button initButton = FindViewById<Button>(Resource.Id.initialize);
+            Button connectButton = FindViewById<Button>(Resource.Id.connect);
+            Button disconnectButton = FindViewById<Button>(Resource.Id.disconnect);
+            Button cycleCameraButton = FindViewById<Button>(Resource.Id.cycleCamera);
+            FrameLayout videoView = FindViewById<FrameLayout>(Resource.Id.videoView);
+
+            initButton.Click += delegate {
+                VidyoClient.ConnectorPKG.SetApplicationUIContext(this);
+                vc.Initialize(videoView.Handle, (uint)videoView.Width, (uint)videoView.Height);
+            };
+            connectButton.Click += delegate { vc.Connect(); };
+            disconnectButton.Click += delegate { vc.Disconnect(); };
+            cycleCameraButton.Click += delegate { vc.CycleCamer(); };
+        }
+    }
+}
---
+++ VidyoDemo/VidyoDemo.Android/VidyoController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using VidyoClient;
+
+namespace VidyoDemo.Droid
+{
+    public class VidyoController : Connector.IConnect, Connector.IRegisterLocalSpeakerEventListener, Connector.IRegisterLocalMicrophoneEventListener
+    {
+        Connector c;
+        List<LocalMicrophone> _localMicrophones = new List<LocalMicrophone>();
+        List<LocalSpeaker> _localSpeakers = new List<LocalSpeaker>();
+        public VidyoController()
+        {
+                
+        }
+
+        public void Initialize(IntPtr viewId, uint width, uint height)
+        {
+            ConnectorPKG.Initialize();
+            c = new Connector(viewId, Connector.ConnectorViewStyle.ConnectorviewstyleDefault, 15, "", "", 0);
+            c.ShowViewAt(viewId, 0, 0, width, height);
+            c.SetMicrophonePrivacy(false);
+            c.SetSpeakerPrivacy(false);
+            c.SelectDefaultCamera();
+            c.SelectDefaultMicrophone();
+            c.SelectDefaultSpeaker();
+            
+        }
+
+        public void Connect()
+        {
+            string token = "";
+            c.Connect("prod.vidyo.io", token, "Sid", "Taritas", this);
+        }
+
+        public void Disconnect()
+        {
+            c.Disconnect();
+        }
+
+        public void CycleCamer()
+        {
+            c.CycleCamera();
+        }
+
+        public void SetMicrophonePrivacy(bool privacy)
+        {
+            c.SetMicrophonePrivacy(privacy);
+        }
+
+        public void OnSuccess()
+        {
+        }
+
+        public void OnFailure(Connector.ConnectorFailReason reason)
+        {
+        }
+
+        public void OnDisconnected(Connector.ConnectorDisconnectReason reason)
+        {
+        }
+        public void OnLocalMicrophoneAdded(LocalMicrophone localMicrophone)
+        {
+            _localMicrophones.Add(localMicrophone);
+        }
+
+        public void OnLocalMicrophoneRemoved(LocalMicrophone localMicrophone)
+        {
+            _localMicrophones.Remove(localMicrophone);
+        }
+
+        public void OnLocalMicrophoneSelected(LocalMicrophone localMicrophone)
+        {
+            
+        }
+
+        public void OnLocalMicrophoneStateUpdated(LocalMicrophone localMicrophone, Device.DeviceState state)
+        {
+
+        }
+
+        public void OnLocalSpeakerAdded(LocalSpeaker localSpeaker)
+        {
+            _localSpeakers.Add(localSpeaker);
+        }
+
+        public void OnLocalSpeakerRemoved(LocalSpeaker localSpeaker)
+        {
+            _localSpeakers.Remove(localSpeaker);
+        }
+
+        public void OnLocalSpeakerSelected(LocalSpeaker localSpeaker)
+        {
+            
+        }
+
+        public void OnLocalSpeakerStateUpdated(LocalSpeaker localSpeaker, Device.DeviceState state)
+        {
+
+        }
+    }
+}
